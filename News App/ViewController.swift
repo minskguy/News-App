005cb33c321418b7275771867e329baaac42d69d
Mark <macvik17@mail.ru>
2021-01-19 19:38:03 +0300

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Марк Курлович on 1/19/21.
+//  Copyright © 2021 Марк Курлович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
